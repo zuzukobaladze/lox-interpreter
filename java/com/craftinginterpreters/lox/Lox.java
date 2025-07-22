3bc01a55407829565164a1c98cee11bf1bfc4bd4
zuzukobaladze <zkobaladze002@gmail.com>
2025-07-22 13:49:30 +0400

    Adding functionalities to Scanner class

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
@@ -50,7 +51,7 @@ public class Lox {
       }
     }
 
-    private static void error(int line, String message) {
+    static void error(int line, String message) {
       report(line, "", message);
     }
 
